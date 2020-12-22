2a529541e75c1a81a04a9fc8df047be9e5594635
anas <mohammedanas359@gmail.com>
2020-12-22 11:56:34 +0530

    Animation added

--- app/src/main/java/com/example/login_register_animation/MainActivity.java
+++ app/src/main/java/com/example/login_register_animation/MainActivity.java
@@ -3,12 +3,48 @@ package com.example.login_register_animation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.mahfa.dnswitch.DayNightSwitch;
+import com.mahfa.dnswitch.DayNightSwitchListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView sun, dayLand, nightLand;
+    View daySky, nightSky;
+    DayNightSwitch dayNightSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sun= findViewById(R.id.sun);
+        dayLand= findViewById(R.id.day_landscape);
+        nightLand= findViewById(R.id.night_landscape);
+        daySky= findViewById(R.id.day_bg);
+        nightSky= findViewById(R.id.night_bg);
+        dayNightSwitch= findViewById(R.id.day_night_switch);
+
+        sun.setTranslationY(-110);
+
+        dayNightSwitch.setListener(new DayNightSwitchListener() {
+            @Override
+            public void onSwitch(boolean is_night) {
+                if(is_night){
+                    sun.animate().translationY(110).setDuration(1300);
+                    dayLand.animate().alpha(0).setDuration(1300);
+                    daySky.animate().alpha(0).setDuration(1300);
+                }else
+                {
+                    sun.animate().translationY(-110).setDuration(1300);
+                    dayLand.animate().alpha(1).setDuration(1300);
+                    daySky.animate().alpha(1).setDuration(1300);
+
+                }
+            }
+        });
+
     }
 }
