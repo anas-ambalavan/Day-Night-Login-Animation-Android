c0f07f7a151395dc418035feb5a9340d22cdc7fd
anas <mohammedanas359@gmail.com>
2020-12-23 10:23:53 +0530

    pictures changed

--- app/src/main/java/com/example/login_register_animation/MainActivity.java
+++ app/src/main/java/com/example/login_register_animation/MainActivity.java
@@ -11,7 +11,7 @@ import com.mahfa.dnswitch.DayNightSwitchListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView sun, dayLand, nightLand;
+    ImageView sun, moon, dayLand, nightLand;
     View daySky, nightSky;
     DayNightSwitch dayNightSwitch;
 
@@ -21,24 +21,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         sun= findViewById(R.id.sun);
+        moon = findViewById(R.id.moon);
         dayLand= findViewById(R.id.day_landscape);
         nightLand= findViewById(R.id.night_landscape);
         daySky= findViewById(R.id.day_bg);
         nightSky= findViewById(R.id.night_bg);
         dayNightSwitch= findViewById(R.id.day_night_switch);
 
-        sun.setTranslationY(-110);
+        sun.setTranslationY(-250);
+        moon.setTranslationY(300);
 
         dayNightSwitch.setListener(new DayNightSwitchListener() {
             @Override
             public void onSwitch(boolean is_night) {
                 if(is_night){
-                    sun.animate().translationY(110).setDuration(1300);
+                    sun.animate().translationY(250).setDuration(1300);
+                    moon.animate().translationY(-200).setDuration(1300);
                     dayLand.animate().alpha(0).setDuration(1300);
                     daySky.animate().alpha(0).setDuration(1300);
                 }else
                 {
-                    sun.animate().translationY(-110).setDuration(1300);
+                    sun.animate().translationY(-250).setDuration(1300);
+                    moon.animate().translationY(300).setDuration(1300);
                     dayLand.animate().alpha(1).setDuration(1300);
                     daySky.animate().alpha(1).setDuration(1300);
 
